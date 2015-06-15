bb447bfa56799848a4e2759eaf67dd16696eb1c4
Ryan Rante <ryan.arjuan.rante@gmail.com>
2015-06-16 01:52:35 +0700

    I create this for your reference
    Made for reference of menus.

---
+++ menu.cpp
@@ -0,0 +1,42 @@
+//for the menu
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+//made for reference of outline
+//do not make the details of every sub-menu
+void tampil_data(){cout<<"checked\n";}
+void tambah_karakter(){cout<<"checked\n";}
+void kurang_karakter(){cout<<"checked\n";}
+void keluar(){cout<<"checked, then bye\n";exit(EXIT_SUCCESS);}
+
+void menu(){
+  char choice;
+  while(cin>>choice){
+    switch(choice){
+      case '1':
+        tampil_data();
+        break;
+      case '2':
+        tambah_karakter();
+        break;
+      case '3':
+        kurang_karakter();
+        break;
+      case '4':
+        keluar();
+        break;
+      default:
+        try{
+          throw choice;
+        }
+        catch(char choice){
+          cerr<<choice<<endl;
+        }
+    }
+  }
+}
+
+main(){
+  menu();
+}
