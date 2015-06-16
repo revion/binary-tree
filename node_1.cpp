ec7e4a78b3a4ead7d994a16aa59def66e8eb10ea
Sherlock9 <evanlouisr@gmail.com>
2015-06-17 00:15:46 +0700

    Update node_1.cpp

--- node_1.cpp
+++ node_1.cpp
@@ -15,6 +15,18 @@ Node::Node(string newNama, string newUmur, string newAlamat)
   alamat = newAlamat;
 }
 
+Node* getLeftNode() { return left; }
+Node* getRightNode() { return right; }
+string getNama() { return nama; }
+string getUmur() { return umur; }
+string getAlamat() { return alamat; }
+
+void setLeftNode( Node* newNode ) { left = newNode; }
+void setRightNode( Node* newNode ) { right = newNode; }
+void setNama( string newNama ) { nama = newName; }
+void setUmur( string newUmur ) { umur = newUmur; }
+void setAlamat( string newAlamat ) { alamat = newAlamat; }
+
 void Node::traversal( Node* root )
 {
   if ( root == NULL ) { return; }
