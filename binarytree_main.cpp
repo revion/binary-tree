79d05dd58bcd0b44678a786c600bb35f3d5ec4d6
Sherlock9 <evanlouisr@gmail.com>
2015-06-17 00:07:30 +0700

    Update binarytree_main.cpp

--- binarytree_main.cpp
+++ binarytree_main.cpp
@@ -1,8 +1,7 @@
 #include "node_1.h"
+#include "tree_1.h"
 
 main(){
-  Node data1;
-  string newNama,newUmur,newAlamat;
-  data1.inputNode();
-  data1.printNode();
+  Tree data1("data.txt");
+  data1.root.traverse(root);
 }
